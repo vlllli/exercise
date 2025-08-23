943cbe08caf6ee822c531f7a7780eca72901fa4b
lock <lock181@qq.com>
2025-08-23 22:49:34 +0800

    review again,tomorrow i will try something new

---
+++ elif.py
@@ -0,0 +1,9 @@
+age=float(input("how old are you?"))
+if age>=60:
+    print("you are older")
+elif age>=35:
+    print("you are old")
+elif age>=18:
+    print("you are adult")
+else:
+    print("how are you,child?")
