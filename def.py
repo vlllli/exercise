61b8bb80f43149031fef9b45c8f6c8a596d6b9b6
lock <lock181@qq.com>
2025-08-25 16:55:04 +0800

    'def'sentence,new for me

---
+++ def.py
@@ -0,0 +1,11 @@
+def print_header(text):
+    print("    ==          ==")
+    print("  ==  ==      ==  ==")
+    print(" ==     ==   ==     ==")
+
+    print(                                 text   )
+
+    print("    ==    ==    ==")
+    print("     == == == ==")
+print_header("thanks for using")
+
